33d9e6e0ca27b3f07340ceb42b572e03fe4cc313
Rob Tatro <tatro009@gmail.com>
2024-02-20 17:39:44 -0600

    Start on routes and views

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+// Create a new post
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedPost = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!updatedPost[0]) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a post
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deletedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
